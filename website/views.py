459130944ccdee13454ffacd4ac6321aab0273f1
Albert Kuc <albert.kuc@gmail.com>
2021-06-22 00:52:52 +0100

    feat: Add delete button to notes list at home page
    Changes to home.html:
    Assing a "close" button to reference delete_note() once onclick.
    
    Changes to views.py
    Define delete_note() to locate a Note object with passed id and delete
    it from database.
    
    Add static/index.js:
    Define javascript delete_note() to pass note_id to delete_note() in views.py and
    once received a response to reload the window.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 """ Define that this file is a blueprint of our application which means it has a bunch of routes inside (urls) """
 views = Blueprint('views', __name__)
@@ -23,3 +24,18 @@ def home():
 
     # reference a current user in base template and check if it's authenticated
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    """
+    Take in data from post request and load as json object, access note_id attribute, look up Note with that id,
+    if Note exists and current user owns this Note, delete it. Return an empty response.
+    """
+    note = json.loads(request.data)
+    note_id = note['note_id']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
