c3835db1141ede6080d4971b0a7ca2484a031291
Albert Kuc <albert.kuc@gmail.com>
2021-06-22 00:16:56 +0100

    feat: Add text field to add notes at home page
    Changes to home.html:
    Add field to display existing notes for logged user.
    Add textarea to type in a new note and button to submit.
    
    Changes to views.py
    Create a new Note object in home() and add to database once
    request.method=='POST'.

--- website/views.py
+++ website/views.py
@@ -1,12 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 """ Define that this file is a blueprint of our application which means it has a bunch of routes inside (urls) """
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required  # with this decorator we cannot access home page without user logged in
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
     # reference a current user in base template and check if it's authenticated
     return render_template("home.html", user=current_user)
